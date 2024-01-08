eb6e3549e3802e96cf87a9da501106aa20b17ee5
Petri Pöllänen <petri.pollanen@atea.fi>
2024-01-08 14:46:37 +0200

    Addded nsg rule to NSG module v1.0.0

--- nsg/v1.0.0/main.tf
+++ nsg/v1.0.0/main.tf
@@ -4,6 +4,21 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg_name
 }
 
+resource "azurerm_network_security_rule" "nsg_rule" {
+  for_each = var.nsg_rules
+  resource_group_name = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  name = each.value.name
+  priority = each.value.priority
+  direction = each.value.direction
+  access = each.value.access
+  protocol = each.value.protocol
+  source_port_range = each.value.source_port_range
+  source_address_prefix = each.value.source_address_prefix
+  destination_port_range = each.value.destination_port_range
+  destination_address_prefix = each.value.destination_address_prefix
+}
+
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   for_each = var.nsg_subnets
   subnet_id = "${var.vnet_id}/subnets/${each.value}"
--- nsg/v1.0.0/variables.tf
+++ nsg/v1.0.0/variables.tf
@@ -21,3 +21,18 @@ variable "nsg_subnets" {
   type        = set(string)
   description = "Subnet ids"
 }
+
+variable "nsg_rules" {
+  description = "Network Security Group rules"
+  type = map(object({
+    name = string
+    priority = number
+    direction = string
+    access = string
+    protocol = string
+    source_port_range = string
+    destination_port_range = string
+    source_address_prefix = string
+    destination_address_prefix = string
+  }))
+}
