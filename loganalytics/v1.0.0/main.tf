b0c785c99c0ca7f75fbd39bee418f6659826bbad
Petri Pöllänen <petri.pollanen@atea.fi>
2024-01-10 15:04:27 +0200

    Added data collection endpoint to log analytics module v1.0.0

--- loganalytics/v1.0.0/main.tf
+++ loganalytics/v1.0.0/main.tf
@@ -5,3 +5,16 @@ resource "azurerm_log_analytics_workspace" "log" {
   sku                 = var.sku
   retention_in_days   = var.retention
 }
+
+resource "azurerm_monitor_data_collection_endpoint" "dce" {
+  for_each = var.dces
+  location = var.location
+  resource_group_name = var.resource_group_name
+  name = "dcre-${each.value.name}"
+}
+
+/*
+resource "azurerm_monitor_data_collection_rule" "dcr" {
+  
+}
+*/
--- loganalytics/v1.0.0/variables.tf
+++ loganalytics/v1.0.0/variables.tf
@@ -23,3 +23,29 @@ variable "retention" {
   description = "Log Analytics Workspace retention in days"
   default = 30
 }
+
+variable "dces" {
+  type = map(object({
+    name = string
+  }))
+}
+
+/*
+variable "dcrs" {
+  type = map(object({
+    name = string
+    data_collection_endpoint_id
+    subnet_name = string
+    nic = string
+    pip_enable = bool
+    ama_enable = bool
+    auto_shutdown_enable = bool
+    auto_shutdown_time = string
+    auto_shutdown_timezone = string
+    source_image_publisher = string
+    source_image_offer = string
+    source_image_sku = string
+    source_image_version = string
+  }))
+}
+*/
